cc31ae73c8c1ebb4ec477b6d63d21c963ac9c333
KevinRamirez <kevin.andrade@bcr.gob.sv>
2026-09-22 10:26:38 -0600

    cambios 21/09

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyparse = require('body-parse');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log(PORT);
 
@@ -16,6 +17,24 @@ app.get('/', (req, res) => {
         <p>Corre en el puerto ${PORT}</p>       
     `);
 });
+
+
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+  const terms = req.query.termino || "no especificado";
+  const category = req.query.categoria || "todas";
+  
+  res.send(`<h2>Resultados de búsqueda</h2>
+            <p>Término: ${terms}</p>
+            <p>Categoría: ${category}</p>`);
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor http://localhost:${PORT}`)
